37404beb194ca02ff078cd6abac91efc90176067
ryadav96 <rec.nita@gmail.com>
2018-08-04 09:56:18 +0530

    Project Web Chat Application Version 1.0

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var socket = require('socket.io');
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+// App setup
+var app = express();
+var server =app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//var server = app.listen(4000, function(){
+//    console.log('listening for requests on port 4000,');
+//});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat',function(data){
+    	io.sockets.emit('chat',data);
+    });
+    socket.on('typing',function(data){
+    	socket.broadcast.emit('typing',data);
+    });
+
+});
